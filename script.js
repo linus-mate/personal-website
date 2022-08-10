d46e630bb4610f96d64077eb628d6e3c5912acac
linus mutugi mate <mlynux@gmail.com>
2022-08-10 19:18:53 +0300

    added mouse events

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ document.querySelector('.menu-icon').onclick = function() {
     document.querySelector('.header-menu').classList.toggle('is-active');
 }
 
+function setNewImage(){
+    document.querySelector('.pic').src ="images/20220810_182635.jpg";
+}
+function setOldImage(){
+    document.querySelector('.pic').src ="images/20220810_184217.jpg";
+}
 //$(document).ready(function(){
     //$('.menu-icon').click(function(){
        // $('.menu-list').toggleClass('show');
